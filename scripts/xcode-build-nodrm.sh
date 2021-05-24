a96199cc6bc7d46eafedc37720356a8b6a681a21
Vladimir Fedorov <fedorovvs@gmail.com>
2021-05-24 18:57:49 +0500

    change project target name

--- scripts/xcode-build-nodrm.sh
+++ scripts/xcode-build-nodrm.sh
@@ -16,7 +16,7 @@ set -eo pipefail
 echo "Building SimplyE without DRM support..."
 
 xcodebuild -project Palace.xcodeproj \
-           -scheme SimplyE-noDRM \
+           -scheme Palace-noDRM \
            -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11\ Pro\
            clean build | \
            if command -v xcpretty &> /dev/null; then xcpretty; else cat; fi
