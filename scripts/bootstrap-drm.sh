8949c517ba02c830eb9c355c2759965612e010da
Vladimir Fedorov <fedorovvs@gmail.com>
2021-06-05 02:00:13 +0500

    fix repository names

--- scripts/bootstrap-drm.sh
+++ scripts/bootstrap-drm.sh
@@ -12,10 +12,10 @@
 #
 
 cd ..
-git clone git@github.com:NYPL-Simplified/Certificates.git
-git clone git@github.com:NYPL-Simplified/DRM-iOS-AdeptConnector.git
+git clone git@github.com:ThePalaceProject/mobile-certificates.git
+git clone git@github.com:ThePalaceProject/android-drm-adeptconnector.git
 
-cd Simplified-iOS
+cd ios-core
 git checkout develop
 
 ./scripts/setup-repo-drm.sh
