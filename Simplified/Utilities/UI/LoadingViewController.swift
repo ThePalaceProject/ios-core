bdd7b8441bd4b92e147b5c93dd58b9cbcafd013c
mauricecarrier7 <Maurice.Carrier@outlook.com>
2021-06-10 10:45:03 -0400

    Implement loading view controller

---
+++ Simplified/Utilities/UI/LoadingViewController.swift
@@ -0,0 +1,49 @@
+//
+//  UIViewController+Loading.swift
+//  SimplyE
+//
+//  Created by Maurice Work on 6/9/21.
+//  Copyright © 2021 NYPL Labs. All rights reserved.
+//
+
+import UIKit
+
+protocol LoadingViewController: UIViewController {
+  var loadingView: UIView? { get set }
+}
+
+extension LoadingViewController {
+  
+  private func loadingOverlayView() -> UIView {
+    let overlayView = UIView()
+    overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+    let activityView = UIActivityIndicatorView(style: .whiteLarge)
+    overlayView.addSubview(activityView)
+    activityView.autoCenterInSuperviewMargins()
+    overlayView.translatesAutoresizingMaskIntoConstraints = false
+    activityView.startAnimating()
+    return overlayView
+  }
+  
+  func startLoading() {
+    guard loadingView == nil else { return }
+    
+    let loadingOverlay = loadingOverlayView()
+    if !Thread.isMainThread {
+      DispatchQueue.main.async {
+        UIApplication.shared.keyWindow?.addSubview(loadingOverlay)
+        loadingOverlay.autoPinEdgesToSuperviewEdges()
+      }
+    } else {
+      UIApplication.shared.keyWindow?.addSubview(loadingOverlay)
+      loadingOverlay.autoPinEdgesToSuperviewEdges()
+    }
+    
+    loadingView = loadingOverlay
+  }
+  
+  func stopLoading() {
+    loadingView?.removeFromSuperview()
+    loadingView = nil
+  }
+}
