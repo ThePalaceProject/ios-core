45b387e3bf1e9d8d4b136dfd8d7f1d64dd2f4845
Vladimir Fedorov <fedorovvs@gmail.com>
2021-06-17 18:03:30 +0500

    Trying to build with disabled bitcode

--- scripts/xcode-test.sh
+++ scripts/xcode-test.sh
@@ -20,6 +20,8 @@ echo "Running unit tests for Palace..."
 
 xcodebuild -project "$PROJECT_NAME" \
            -scheme "$SCHEME" \
+           ENABLE_BITCODE=0 \
+           LD_VERIFY_BITCODE=NO \
            -destination platform=iOS\ Simulator,OS=13.7,name=iPhone\ 11 \
            clean test | \
            if command -v xcpretty &> /dev/null; then xcpretty; else cat; fi
