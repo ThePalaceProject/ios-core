f6f52b2f20bb4c46efc760d7a7f899817126bc59
mauricecarrier7 <Maurice.Carrier@outlook.com>
2021-06-18 17:15:31 -0400

    Update TPPSettingsAccountList and TPPWelcomeSCreenViewController to use new TPPAccountList and TPPLoadingViewController

--- Palace/WelcomeScreen/TPPWelcomeScreenViewController.swift
+++ Palace/WelcomeScreen/TPPWelcomeScreenViewController.swift
@@ -3,9 +3,10 @@ import PureLayout
 
 
 /// Welcome screen for a first-time user
-@objcMembers final class TPPWelcomeScreenViewController: UIViewController {
+@objcMembers final class TPPWelcomeScreenViewController: UIViewController, TPPLoadingViewController {
   
   var completion: ((Account) -> ())?
+  var loadingView: UIView?
   
   required init(completion: ((Account) -> ())?) {
     self.completion = completion
@@ -136,35 +137,6 @@ import PureLayout
     let alert = TPPAlertUtils.alert(title:nil, message:"We can’t get your library right now. Please close and reopen the app to try again.", style: .cancel)
     present(alert, animated: true, completion: nil)
   }
-  
-  func loadingOverlayView() -> UIView {
-    let overlayView = UIView()
-    overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
-    let activityView = UIActivityIndicatorView(style: .whiteLarge)
-    overlayView.addSubview(activityView)
-    activityView.autoCenterInSuperviewMargins()
-    activityView.startAnimating()
-    return overlayView
-  }
-  
-  private func addLoadingOverlayView(toVC viewController: UIViewController? = nil) -> UIView {
-    let vc = viewController ?? self
-    let loadingOverlay = loadingOverlayView()
-    if !Thread.isMainThread {
-      DispatchQueue.main.async {
-        vc.view.addSubview(loadingOverlay)
-        loadingOverlay.autoPinEdgesToSuperviewEdges()
-      }
-    } else {
-      vc.view.addSubview(loadingOverlay)
-      loadingOverlay.autoPinEdgesToSuperviewEdges()
-    }
-    return loadingOverlay
-  }
-  
-  private func removeLoadingOverlayView(_ view: UIView?) {
-    view?.removeFromSuperview()
-  }
 
   func pickYourLibraryTapped() {
     if completion == nil {
@@ -173,15 +145,15 @@ import PureLayout
     }
     
     let pickLibrary = {
-      let listVC = TPPWelcomeScreenAccountList { account in
+      let listVC = TPPAccountList { account in
         if account.details != nil {
           self.completion?(account)
         } else {
           // Show loading overlay while we load the auth document
-          let loadingOverlay = self.addLoadingOverlayView(toVC: self.navigationController)
+          self.startLoading()
           account.loadAuthenticationDocument { (success) in
             DispatchQueue.main.async {
-              self.removeLoadingOverlayView(loadingOverlay)
+              self.stopLoading()
               guard success else {
                 self.showLoadingFailureAlert()
                 return
@@ -203,10 +175,10 @@ import PureLayout
       pickLibrary()
     } else {
       // Show loading overlay while loading library list, which is required for pickLibrary
-      let loadingOverlay = addLoadingOverlayView()
+      startLoading()
       AccountsManager.shared.loadCatalogs() { (success) in
         DispatchQueue.main.async {
-          self.removeLoadingOverlayView(loadingOverlay)
+          self.stopLoading()
           guard success else {
             self.showLoadingFailureAlert()
             return
@@ -219,24 +191,21 @@ import PureLayout
 
   func instantClassicsTapped() {
     let classicsId = AccountsManager.TPPAccountUUIDs[2]
-    var loadingOverlay: UIView? = nil
     
     let selectInstantClassics = {
       guard let classicsAccount = AccountsManager.shared.account(classicsId) else {
         DispatchQueue.main.async {
-          self.removeLoadingOverlayView(loadingOverlay)
+          self.stopLoading()
           self.showLoadingFailureAlert()
         }
         return
       }
-      // If we didn't add the loading overlay to load the library list, we need to add it now to load the auth document
-      if loadingOverlay == nil {
-        loadingOverlay = self.addLoadingOverlayView()
-      }
+      //Show indicator while loading the auth document
+      self.startLoading()
       // Load the auth document for the classics library
       classicsAccount.loadAuthenticationDocument { (authSuccess) in
         DispatchQueue.main.async {
-          self.removeLoadingOverlayView(loadingOverlay)
+          self.stopLoading()
           if authSuccess {
             TPPSettings.shared.settingsAccountsList = [classicsId]
             self.completion?(AccountsManager.shared.account(classicsId)!)
@@ -251,139 +220,17 @@ import PureLayout
       selectInstantClassics()
     } else {
       // Make sure the library list is loaded
-      loadingOverlay = addLoadingOverlayView()
+      startLoading()
       AccountsManager.shared.loadCatalogs() { success in
         DispatchQueue.main.async {
           if success {
             selectInstantClassics()
           } else {
-            self.removeLoadingOverlayView(loadingOverlay)
+            self.stopLoading()
             self.showLoadingFailureAlert()
           }
         }
       }
     }
-  }
-}
-
-
-/// List of available Libraries/Accounts to select as patron's primary
-/// when going through Welcome Screen flow.
-final class TPPWelcomeScreenAccountList: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  
-  var accounts: [Account]!
-  var nyplAccounts: [Account]!
-  let completion: (Account) -> ()
-  weak var tableView : UITableView!
-  
-  required init(completion: @escaping (Account) -> ()) {
-    self.completion = completion
-    super.init(nibName:nil, bundle:nil)
-  }
-  
-  @available(*, unavailable)
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func viewDidLoad() {
-    self.view = UITableView(frame: .zero, style: .grouped)
-    self.tableView = self.view as? UITableView
-    self.tableView.delegate = self
-    self.tableView.dataSource = self
-    
-    self.accounts = AccountsManager.shared.accounts()
-
-    //FIXME Replace with SettingsAccounts improvements to library selection VC
-    //once that gets finalized and merged in.
-    self.accounts.sort { $0.name < $1.name }
-    self.nyplAccounts = self.accounts.filter { AccountsManager.TPPAccountUUIDs.contains($0.uuid) }
-    self.accounts = self.accounts.filter { !AccountsManager.TPPAccountUUIDs.contains($0.uuid) }
-
-    self.title = NSLocalizedString("Pick Your Library", comment: "Title that also informs the user that they should choose a library from the list.")
-    self.view.backgroundColor = TPPConfiguration.backgroundColor()
-  }
-  
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 100
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.section == 0 {
-      completion(nyplAccounts[indexPath.row])
-    } else {
-      completion(accounts[indexPath.row])
-    }
-  }
-
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 2
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if section == 0 {
-      return self.nyplAccounts.count
-    } else {
-      return self.accounts.count
-    }
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if indexPath.section == 0 {
-      return cellForLibrary(self.nyplAccounts[indexPath.row])
-    } else {
-      return cellForLibrary(self.accounts[indexPath.row])
-    }
-  }
-  
-  func cellForLibrary(_ account: Account) -> UITableViewCell {
-    let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "")
-    
-    let container = UIView()
-    let textContainer = UIView()
-
-    cell.accessoryType = .disclosureIndicator
-    let imageView = UIImageView(image: account.logo)
-    imageView.contentMode = .scaleAspectFit
-
-    let textLabel = UILabel()
-    textLabel.font = UIFont.systemFont(ofSize: 16)
-    textLabel.text = account.name
-    textLabel.numberOfLines = 0
-
-    let detailLabel = UILabel()
-    detailLabel.font = UIFont(name: "AvenirNext-Regular", size: 12)
-    detailLabel.numberOfLines = 0
-    detailLabel.text = account.subtitle
-
-    textContainer.addSubview(textLabel)
-    textContainer.addSubview(detailLabel)
-
-    container.addSubview(imageView)
-    container.addSubview(textContainer)
-    cell.contentView.addSubview(container)
-
-    imageView.autoAlignAxis(toSuperviewAxis: .horizontal)
-    imageView.autoPinEdge(toSuperviewEdge: .left)
-    imageView.autoSetDimensions(to: CGSize(width: 45, height: 45))
-
-    textContainer.autoPinEdge(.left, to: .right, of: imageView, withOffset: cell.contentView.layoutMargins.left * 2)
-    textContainer.autoPinEdge(toSuperviewMargin: .right)
-    textContainer.autoAlignAxis(toSuperviewAxis: .horizontal)
-
-    NSLayoutConstraint.autoSetPriority(UILayoutPriority.defaultLow) {
-      textContainer.autoPinEdge(toSuperviewEdge: .top, withInset: 0, relation: .greaterThanOrEqual)
-      textContainer.autoPinEdge(toSuperviewEdge: .bottom, withInset: 0, relation: .greaterThanOrEqual)
-    }
-
-    textLabel.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .bottom)
-
-    detailLabel.autoPinEdge(.top, to: .bottom, of: textLabel)
-    detailLabel.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .top)
-
-    container.autoPinEdgesToSuperviewMargins()
-    container.autoSetDimension(.height, toSize: 55, relation: .greaterThanOrEqual)
-
-    return cell
   }
 }
