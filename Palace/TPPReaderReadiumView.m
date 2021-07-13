6d24d0752788c55a94bdfbec1933c22bb366a17a
mauricecarrier7 <Maurice.Carrier@outlook.com>
2021-07-13 17:03:29 -0400

    Disable copy and share

--- Palace/TPPReaderReadiumView.m
+++ Palace/TPPReaderReadiumView.m
@@ -34,9 +34,9 @@
 
 -(BOOL)canPerformAction:(SEL)action withSender:(id)sender
 {
-  // Note: this does not work on iOS <= 10: the callback is never called for
-  // the copy: action. It is on iOS 11+.
-  if (action == @selector(copy:)) {
+  // Note: this does not work on iOS <= 10.
+  // Also Note: _share is in fact a declared selector
+  if (action == @selector(copy:) || action == @selector(_share:)) {
     return NO;
   }
   return [super canPerformAction:action withSender:sender];
