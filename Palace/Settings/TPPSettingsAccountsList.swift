b6200d3a7007a96b53c4bd6f5d362a984335585a
Vladimir Fedorov <fedorovvs@gmail.com>
2021-08-21 00:55:52 +0500

    Update Palace/Settings/TPPSettingsAccountsList.swift
    Co-authored-by: mauricecarrier <maurice.carrier7@gmail.com>

--- Palace/Settings/TPPSettingsAccountsList.swift
+++ Palace/Settings/TPPSettingsAccountsList.swift
@@ -176,7 +176,6 @@
     }
     updateSettingsAccountList()
     // Return from search screen to the list of libraries
-    tableView.reloadData()
     navigationController?.popViewController(animated: false)
     // Switch to the selected library
     AccountsManager.shared.currentAccount = account
