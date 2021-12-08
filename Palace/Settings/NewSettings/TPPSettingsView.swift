7bac5b15e94da2b2e8a7a3bae3a5e055ee8ef8dc
mauricecarrier7 <Maurice.Carrier@outlook.com>
2021-12-08 13:29:19 -0500

    Revert "Update TPPSettingsView.swift"
    This reverts commit af55ebe1fbf9f6f5adafe160b4216247eb9e9818.

--- Palace/Settings/NewSettings/TPPSettingsView.swift
+++ Palace/Settings/NewSettings/TPPSettingsView.swift
@@ -10,30 +10,15 @@ import SwiftUI
 
 struct TPPSettingsView: View {
   typealias Strings = DisplayStrings.Settings
-
   @State private var showDeveloperSettings = false
-  @State private var selectedView: Int? = 0
-  @State private var orientation: UIDeviceOrientation = UIDevice.current.orientation
-
-  private var sideBarEnabled: Bool {
-    UIDevice.current.userInterfaceIdiom == .pad
-      &&  UIDevice.current.orientation != .portrait
-      &&  UIDevice.current.orientation != .portraitUpsideDown
-  }
 
   var body: some View {
-    if sideBarEnabled {
+    if UIDevice.current.userInterfaceIdiom == .pad {
       NavigationView {
-        listView
-          .onAppear {
-            selectedView = 1
-          }
+          listView
       }
     } else {
       listView
-        .onAppear {
-          selectedView = 0
-        }
     }
   }
 
@@ -45,9 +30,6 @@ struct TPPSettingsView: View {
     }
     .navigationBarTitle(Strings.settings)
     .listStyle(GroupedListStyle())
-    .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-      self.orientation = UIDevice.current.orientation
-    }
   }
 
   @ViewBuilder private var librariesSection: some View {
@@ -62,7 +44,7 @@ struct TPPSettingsView: View {
       .navigationBarItems(trailing: navButton)
     
     Section {
-      row(title: Strings.libraries, index: 1, selection: self.$selectedView, destination: wrapper.anyView())
+      row(title: Strings.libraries, destination: wrapper.anyView())
     }
   }
 
@@ -86,7 +68,7 @@ struct TPPSettingsView: View {
     let wrapper = UIViewControllerWrapper(viewController, updater: { _ in })
       .navigationBarTitle(Text(Strings.aboutApp))
 
-    row(title: Strings.aboutApp, index: 2, selection: self.$selectedView, destination: wrapper.anyView())
+    row(title: Strings.aboutApp, destination: wrapper.anyView())
   }
 
   @ViewBuilder private var privacyRow: some View {
@@ -99,8 +81,7 @@ struct TPPSettingsView: View {
     let wrapper = UIViewControllerWrapper(viewController, updater: { _ in })
       .navigationBarTitle(Text(Strings.privacyPolicy))
     
-    row(title: Strings.privacyPolicy, index: 3, selection: self.$selectedView, destination: wrapper.anyView())
-
+    row(title: Strings.privacyPolicy, destination: wrapper.anyView())
   }
 
   @ViewBuilder private var userAgreementRow: some View {
@@ -113,7 +94,7 @@ struct TPPSettingsView: View {
     let wrapper = UIViewControllerWrapper(viewController, updater: { _ in })
       .navigationBarTitle(Text(Strings.eula))
     
-    row(title: Strings.eula, index: 4, selection: self.$selectedView, destination: wrapper.anyView())
+    row(title: Strings.eula, destination: wrapper.anyView())
   }
 
   @ViewBuilder private var softwareLicenseRow: some View {
@@ -125,7 +106,7 @@ struct TPPSettingsView: View {
     let wrapper = UIViewControllerWrapper(viewController, updater: { _ in })
       .navigationBarTitle(Text(Strings.softwareLicenses))
     
-    row(title: Strings.softwareLicenses, index: 5, selection: self.$selectedView, destination: wrapper.anyView())
+    row(title: Strings.softwareLicenses, destination: wrapper.anyView())
   }
 
   @ViewBuilder private var developerSettingsSection: some View {
@@ -136,7 +117,7 @@ struct TPPSettingsView: View {
         let wrapper = UIViewControllerWrapper(viewController, updater: { _ in })
           .navigationBarTitle(Text(Strings.developerSettings))
         
-        row(title: Strings.developerSettings, index: 6, selection: self.$selectedView, destination: wrapper.anyView())
+        row(title: Strings.developerSettings, destination: wrapper.anyView())
       }
     }
   }
@@ -154,12 +135,9 @@ struct TPPSettingsView: View {
       .horizontallyCentered()
   }
   
-  private func row(title: String, index: Int, selection: Binding<Int?>, destination: AnyView) -> some View {
-    NavigationLink(
-      destination: destination,
-      tag: index,
-      selection: selection,
-      label: { Text(title) }
-    )
+  private func row(title: String, destination: AnyView) -> some View {
+    NavigationLink(destination: destination) {
+      Text(title)
+    }
   }
 }
