bead9ccfb2b3b00eeaba6b357b125c82713d9b58
mauricecarrier7 <Maurice.Carrier@outlook.com>
2022-01-19 10:27:29 -0500

    Restore build

--- scripts/bootstrap-drm.sh
+++ scripts/bootstrap-drm.sh
@@ -14,7 +14,6 @@
 cd ..
 git clone git@github.com:ThePalaceProject/mobile-certificates.git
 git clone git@github.com:ThePalaceProject/mobile-drm-adeptconnector.git
-git clone git@github.com:ThePalaceProject/ios-audiobook-overdrive.git
 
 cd ios-core
 
