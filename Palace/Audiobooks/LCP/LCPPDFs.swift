6cc0b170585baafba0cff514a431c04ad88622ca
mauricecarrier7 <Maurice.Carrier@outlook.com>
2022-03-24 23:10:00 -0400

    Update LCPPDFs.swift

--- Palace/Audiobooks/LCP/LCPPDFs.swift
+++ Palace/Audiobooks/LCP/LCPPDFs.swift
@@ -15,28 +15,11 @@ import ReadiumLCP
 
 /// LCP PDF helper class
 @objc class LCPPDFs: NSObject {
-    
   private static let expectedAcquisitionType = "application/vnd.readium.lcp.license.v1.0+json"
-  
-  private let pdfURL: URL
-  private let lcpService = LCPLibraryService()
-  private let streamer: Streamer
-  
-  /// Initialize for an LCP PDF
-  /// - Parameter pdfURL: must be a file with `.pdf` extension
-  @objc init?(for pdfURL: URL) {
-    // Check contentProtection is in place
-    guard let contentProtection = lcpService.contentProtection else {
-      TPPErrorLogger.logError(nil, summary: "Uninitialized contentProtection in LCPAudiobooks")
-      return nil
-    }
-    self.pdfURL = pdfURL
-    self.streamer = Streamer(contentProtections: [contentProtection])
-  }
-
-  /// Check if the book is LCP audiobook
-  /// - Parameter book: audiobook
-  /// - Returns: `true` if the book is an LCP DRM protected audiobook, `false` otherwise
+ 
+  /// Check if the book is LCPPDF
+  /// - Parameter book: pdf
+  /// - Returns: `true` if the book is an LCP DRM protected PDF, `false` otherwise
   @objc static func canOpenBook(_ book: TPPBook) -> Bool {
     guard let defualtAcquisition = book.defaultAcquisition() else { return false }
     return book.defaultBookContentType() == .PDF && defualtAcquisition.type == expectedAcquisitionType
