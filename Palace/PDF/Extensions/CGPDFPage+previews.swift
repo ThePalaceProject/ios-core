a5a2530e89bfd9b26db16f8394b2a155e0f9faa5
Vladimir Fedorov <fedorovvs@gmail.com>
2022-07-14 17:09:38 +0200

    Fix occasional crashes on start

--- Palace/PDF/Extensions/CGPDFPage+previews.swift
+++ Palace/PDF/Extensions/CGPDFPage+previews.swift
@@ -13,36 +13,34 @@ extension CGPDFPage {
   /// - Parameter rect: Rectangle to render the page to
   /// - Returns: Pendered PDF page image
   func image(of size: CGSize? = nil, for documentBox: CGPDFBox? = .mediaBox) -> UIImage? {
-    autoreleasepool {
-      var pageRect = getBoxRect(documentBox ?? .mediaBox)
-      var pdfScale = 1.0
-      if let size = size {
-        pdfScale = min(size.width / pageRect.size.width, size.height / pageRect.size.height)
-        pageRect.origin = .zero
-        pageRect.size = CGSize(width: pageRect.size.width * pdfScale, height: pageRect.size.height * pdfScale)
-      }
-      
-      UIGraphicsBeginImageContext(pageRect.size);
-
-      guard let context = UIGraphicsGetCurrentContext() else {
-        return nil
-      }
-
-      context.setFillColor(UIColor.white.cgColor)
-      context.fill(pageRect)
-      context.saveGState()
-      
-      context.translateBy(x: 0, y: pageRect.size.height)
-      context.scaleBy(x: 1, y: -1)
-      context.scaleBy(x: pdfScale, y: pdfScale)
-      context.drawPDFPage(self)
-      context.restoreGState()
-
-      let image = UIGraphicsGetImageFromCurrentImageContext()
-      
-      UIGraphicsEndImageContext()
-      return image
+    var pageRect = getBoxRect(documentBox ?? .mediaBox)
+    var pdfScale = 1.0
+    if let size = size {
+      pdfScale = min(size.width / pageRect.size.width, size.height / pageRect.size.height)
+      pageRect.origin = .zero
+      pageRect.size = CGSize(width: pageRect.size.width * pdfScale, height: pageRect.size.height * pdfScale)
     }
+    
+    UIGraphicsBeginImageContext(pageRect.size);
+    
+    guard let context = UIGraphicsGetCurrentContext() else {
+      return nil
+    }
+    
+    context.setFillColor(UIColor.white.cgColor)
+    context.fill(pageRect)
+    context.saveGState()
+    
+    context.translateBy(x: 0, y: pageRect.size.height)
+    context.scaleBy(x: 1, y: -1)
+    context.scaleBy(x: pdfScale, y: pdfScale)
+    context.drawPDFPage(self)
+    context.restoreGState()
+    
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
+    return image
   }
   
   /// Thumbnail image of the page
