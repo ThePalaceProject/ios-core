e5096c20957c4c9bf1c79ed9d52c185b4d044322
Vladimir Fedorov <fedorovvs@gmail.com>
2022-07-15 16:59:50 +0200

    Revert "Ad documentation"
    This reverts commit 003601bc07858dc46aac95fb1a46f359b449ba92.

--- Palace/PDF/Model/TPPPDFDocument.swift
+++ Palace/PDF/Model/TPPPDFDocument.swift
@@ -121,9 +121,6 @@ extension TPPPDFDocument {
     document?.page(at: page)?.bounds(for: .mediaBox).size
   }
   
-  /// Page label
-  /// - Parameter page: Page number
-  /// - Returns: Page label
   func label(page: Int) -> String? {
     isEncrypted ?
     encryptedDocument?.page(at: page)?.pageNumber.description :
