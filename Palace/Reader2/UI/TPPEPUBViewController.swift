f49ac839a2f0443382e99aa66ef8695c9b82ee0d
mauricecarrier <Maurice.Carrier@outlook.com>
2022-12-09 12:12:54 -0500

    Update navigation bar when appearance is changed (#226)
    * trigger navbar to force update
    
    * Update project.pbxproj
    
    * Update status bar when navbar is updated
    
    * Update TPPEPUBViewController.swift

--- Palace/Reader2/UI/TPPEPUBViewController.swift
+++ Palace/Reader2/UI/TPPEPUBViewController.swift
@@ -100,7 +100,11 @@ class TPPEPUBViewController: TPPBaseReaderViewController {
 
   override open func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
-    navigationController?.navigationBar.setAppearance(TPPConfiguration.defaultAppearance())
+    if let appearance = TPPConfiguration.defaultAppearance() {
+      navigationController?.navigationBar.setAppearance(appearance)
+      navigationController?.navigationBar.forceUpdateAppearance(style: appearance.backgroundColor == .black ? .light : .dark)
+    }
+
     navigationController?.navigationBar.tintColor = TPPConfiguration.iconColor()
     tabBarController?.tabBar.tintColor = TPPConfiguration.iconColor()
     epubNavigator.userSettings.save()
@@ -161,6 +165,7 @@ extension TPPEPUBViewController: TPPReaderSettingsDelegate {
     view.backgroundColor = colors.backgroundColor
     view.tintColor = colors.textColor
     navigationController?.navigationBar.setAppearance(TPPConfiguration.appearance(withBackgroundColor: colors.backgroundColor))
+    navigationController?.navigationBar.forceUpdateAppearance(style: colors.navigationColor == .black ? .light : .dark)
     navigationController?.navigationBar.tintColor = colors.navigationColor
     tabBarController?.tabBar.tintColor = colors.navigationColor
   }
