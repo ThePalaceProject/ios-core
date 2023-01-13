e2beb59920a75f634fddf0a050872031c286bcd2
Vladimir Fedorov <fedorovvs@gmail.com>
2023-01-13 17:33:21 +0100

    Add push notifications support

--- Palace/AppInfrastructure/TPPAppDelegate.m
+++ Palace/AppInfrastructure/TPPAppDelegate.m
@@ -49,8 +49,6 @@ didFinishLaunchingWithOptions:(__attribute__((unused)) NSDictionary *)launchOpti
   [TransifexManager setup];
   [app setMinimumBackgroundFetchInterval:MinimumBackgroundFetchInterval];
 
-  self.notificationsManager = [[TPPUserNotifications alloc] init];
-  [self.notificationsManager authorizeIfNeeded];
   [NSNotificationCenter.defaultCenter addObserver:self
                                          selector:@selector(signingIn:)
                                              name:NSNotification.TPPIsSigningIn
@@ -83,6 +81,9 @@ didFinishLaunchingWithOptions:(__attribute__((unused)) NSDictionary *)launchOpti
   // Initialize TPPBookRegistry
   [TPPBookRegistry shared];
   
+  // Push Notificatoins
+  [[NotificationService sharedService] setupPushNotifications];
+
   return YES;
 }
 
