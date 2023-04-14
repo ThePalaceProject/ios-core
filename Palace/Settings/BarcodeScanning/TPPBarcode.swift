9a02f094e12effe4187a8dfafd48d3eab10aadc1
Vladimir Fedorov <fedorovvs@gmail.com>
2023-04-14 20:41:53 +0200

    Add barcode scanner

--- Palace/Settings/BarcodeScanning/TPPBarcode.swift
+++ Palace/Settings/BarcodeScanning/TPPBarcode.swift
@@ -1,29 +1,30 @@
 import Foundation
-import ZXingObjC
+//import ZXingObjC
+import AVFoundation
 
 fileprivate let barcodeHeight: CGFloat = 100
 fileprivate let maxBarcodeWidth: CGFloat = 414
 
-@objc enum NYPLBarcodeType: Int {
-  case codabar
-  case code39
-  case qrCode
-  case code128
-}
-
-fileprivate func ZXBarcodeFormatFor(_ NYPLBarcodeType:NYPLBarcodeType) -> ZXBarcodeFormat {
-  switch NYPLBarcodeType {
-  case .codabar:
-    return kBarcodeFormatCodabar
-  case .code39:
-    return kBarcodeFormatCode39
-  case .qrCode:
-    return kBarcodeFormatQRCode
-  case .code128:
-    return kBarcodeFormatCode128
-  }
-}
-
+//@objc enum NYPLBarcodeType: Int {
+//  case codabar
+//  case code39
+//  case qrCode
+//  case code128
+//}
+//
+//fileprivate func ZXBarcodeFormatFor(_ NYPLBarcodeType:NYPLBarcodeType) -> ZXBarcodeFormat {
+//  switch NYPLBarcodeType {
+//  case .codabar:
+//    return kBarcodeFormatCodabar
+//  case .code39:
+//    return kBarcodeFormatCode39
+//  case .qrCode:
+//    return kBarcodeFormatQRCode
+//  case .code128:
+//    return kBarcodeFormatCode128
+//  }
+//}
+//
 /// Manage creation and scanning of barcodes on library cards.
 /// Keep any third party dependency abstracted out of the main app.
 @objcMembers final class TPPBarcode: NSObject {
@@ -35,31 +36,12 @@ fileprivate func ZXBarcodeFormatFor(_ NYPLBarcodeType:NYPLBarcodeType) -> ZXBarc
     self.libraryName = library
   }
 
-  func image(fromString stringToEncode: String, superviewWidth: CGFloat, type: NYPLBarcodeType) -> UIImage?
-  {
-    let barcodeWidth = imageWidthFor(superviewWidth)
-    let encodeHints = ZXEncodeHints.init()
-    encodeHints.margin = 0
-    if let image = TPPZXingEncoder.encode(with: stringToEncode,
-                                           format: ZXBarcodeFormatFor(type),
-                                           width: Int32(barcodeWidth),
-                                           height: Int32(barcodeHeight),
-                                           library: self.libraryName ?? "Unknown",
-                                           encodeHints: encodeHints)
-    {
-      return image
-    } else {
-      Log.error(#file, "Failed to create barcode image.")
-      return nil
-    }
-  }
-
   class func presentScanner(withCompletion completion: @escaping (String?) -> ())
   {
     AVCaptureDevice.requestAccess(for: .video) { granted in
       DispatchQueue.main.async {
         if granted {
-          guard let scannerVC = TPPBarcodeScanningViewController.init(completion: completion) else { return }
+          let scannerVC = BarcodeScanner(completion: completion)
           let navController = UINavigationController.init(rootViewController: scannerVC)
           TPPRootTabBarController.shared().safelyPresentViewController(navController, animated: true, completion: nil)
         } else {
