ae7887bfa099487680baa9f680a33b90bd29f354
Vladimir Fedorov <fedorovvs@gmail.com>
2023-11-24 23:17:35 +0100

    Add PalaceUIKit library

---
+++ PalaceUIKit/PalaceUIKit.h
@@ -0,0 +1,19 @@
+//
+//  PalaceUIKit.h
+//  PalaceUIKit
+//
+//  Created by Vladimir Fedorov on 22.11.2023.
+//  Copyright © 2023 The Palace Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PalaceUIKit.
+FOUNDATION_EXPORT double PalaceUIKitVersionNumber;
+
+//! Project version string for PalaceUIKit.
+FOUNDATION_EXPORT const unsigned char PalaceUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PalaceUIKit/PublicHeader.h>
+
+
