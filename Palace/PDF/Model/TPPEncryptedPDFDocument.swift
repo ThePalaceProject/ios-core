c1af0af710865a1e84fa90edc64fb72502becaee
Vladimir Fedorov <fedorovvs@gmail.com>
2023-11-30 00:46:40 +0100

    Move parser code to TPPPDFTextExtractor

--- Palace/PDF/Model/TPPEncryptedPDFDocument.swift
+++ Palace/PDF/Model/TPPEncryptedPDFDocument.swift
@@ -78,12 +78,13 @@ import UIKit
       guard let page = document.page(at: pageNumber) else {
         continue
       }
-      let parser = TPPPDFParser()
-      let textBlocks = parser.extractText(page: page).map { $0.lowercased() }
-      for textBlock in textBlocks {
+      let extractor = TPPPDFTextExtractor()
+      let textBlocks = extractor.extractText(page: page)
+      let lowercaseBlocks = textBlocks.map { $0.lowercased() }
+      for (i, textBlock) in lowercaseBlocks.enumerated() {
         if textBlock.contains(text) {
           // ! CGPDF first page index is 1, that's why we subtract 1 from pageNumber
-          result.append(TPPPDFLocation(title: textBlock, subtitle: nil, pageLabel: nil, pageNumber: page.pageNumber - 1))
+          result.append(TPPPDFLocation(title: textBlocks[i], subtitle: nil, pageLabel: nil, pageNumber: page.pageNumber - 1))
         }
       }
     }
@@ -161,104 +162,5 @@ extension TPPEncryptedPDFDocument {
     TPPEncryptedPDFDocument(encryptedData: Data()) { data, start, end in
       data
     }
-  }
-}
-
-// MARK: - PDF Page parser
-
-fileprivate class TPPPDFParser {
-  private var textBlocks = [String]()
-  // Extracts blocks of text
-  // One block is not neccessarily a sentences, a line of text, or even one word -
-  // depending on the software used, it can contain one-two letters only.
-  func extractText(page: CGPDFPage) -> [String] {
-    let stream = CGPDFContentStreamCreateWithPage(page)
-    let operatorTable = CGPDFOperatorTableCreate()
-    // Documentation:
-    // https://developer.apple.com/documentation/coregraphics/1454118-cgpdfoperatortablesetcallback
-    // PDF operators:
-    // https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/pdfreference1.3.pdf
-    // "TJ" operator: an array of blocks (strings, numbers, etc)
-    CGPDFOperatorTableSetCallback(operatorTable!, "TJ") { scanner, context in
-      guard let context = context else { return }
-      let extractor = Unmanaged<TPPPDFParser>.fromOpaque(context).takeUnretainedValue()
-      extractor.handleArray(scanner: scanner)
-    }
-    // String operators
-    for op in ["Tj", "\"", "'"] {
-      CGPDFOperatorTableSetCallback(operatorTable!, op) { scanner, context in
-        guard let context = context else { return }
-        let extractor = Unmanaged<TPPPDFParser>.fromOpaque(context).takeUnretainedValue()
-        extractor.handleString(scanner: scanner)
-      }
-    }
-    let scanner = CGPDFScannerCreate(stream, operatorTable, Unmanaged.passUnretained(self).toOpaque())
-    CGPDFScannerScan(scanner)
-    return textBlocks
-  }
-  
-  /// String operator handler
-  /// - Parameter scanner: `CGPDFScannerRef`
-  private func handleString(scanner: CGPDFScannerRef) {
-    var pdfString: CGPDFStringRef?
-    if CGPDFScannerPopString(scanner, &pdfString), let pdfString, let cfString = CGPDFStringCopyTextString(pdfString) {
-      let string = cfString as String
-      // Skip control sequences
-      if !string[string.startIndex].isWhitespace {
-        textBlocks.append(string)
-      }
-    }
-  }
-  
-  /// Array operator handler
-  /// - Parameter scanner: `CGPDFScannerRef`
-  private func handleArray(scanner: CGPDFScannerRef) {
-    var array: CGPDFArrayRef?
-    guard CGPDFScannerPopArray(scanner, &array), let array else { return }
-    
-    var blockValue = ""
-    // Iterate through the array elements
-    let count = CGPDFArrayGetCount(array)
-    for index in 0..<count {
-      var obj: CGPDFObjectRef?
-      guard CGPDFArrayGetObject(array, index, &obj), let obj else { continue }
-      
-      let type = CGPDFObjectGetType(obj)
-      switch type {
-      case .string:
-        // Extract and append the string to the text
-        var pdfString: CGPDFStringRef?
-        if CGPDFObjectGetValue(obj, .string, &pdfString), let pdfString, let cfString = CGPDFStringCopyTextString(pdfString) {
-          let string = cfString as String
-          // Skip control sequences
-          if !string[string.startIndex].isWhitespace {
-            print(">> \(string)")
-            blockValue += string
-          }
-        }
-      case .real:
-        var realValue: CGPDFReal = 0.0
-        if CGPDFObjectGetValue(obj, .real, &realValue) {
-          // Real values adjust the spacing between elements (e.g., letters).
-          // "100" is an empirical value large enough to represent a space between words.
-          // Text in PDFs can appear as a single line of characters without whitespace characters;
-          // these values adjust the visual spacing between characters.
-          if abs(realValue) > 100 {
-            blockValue += " "
-          }
-        }
-      case .integer:
-        var intValue: CGPDFInteger = 0
-        if CGPDFObjectGetValue(obj, .integer, &intValue) {
-          // The same as realValue above
-          if abs(intValue) > 100 {
-            blockValue += " "
-          }
-        }
-      default:
-        break
-      }
-    }
-    textBlocks.append(blockValue)
   }
 }
