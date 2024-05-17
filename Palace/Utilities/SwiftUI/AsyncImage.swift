437ba76c28f917dc8de0674ed64a516f8d222137
Maurice Carrier <maurice.carrier7@gmail.com>
2024-05-17 14:41:32 -0400

    Improve image mapping and other performance/threading improvements

--- Palace/Utilities/SwiftUI/AsyncImage.swift
+++ Palace/Utilities/SwiftUI/AsyncImage.swift
@@ -11,38 +11,38 @@ import Combine
 import Foundation
 
 class ImageLoader {
-    private let urlSession: URLSession
-
-    init(urlSession: URLSession = .shared) {
-        self.urlSession = urlSession
-    }
-
-    func publisher(for url: URL) -> AnyPublisher<UIImage, Error> {
-        urlSession.dataTaskPublisher(for: url)
-            .map(\.data)
-            .tryMap { data in
-                guard let image = UIImage(data: data) else {
-                    throw URLError(.badServerResponse, userInfo: [
-                        NSURLErrorFailingURLErrorKey: url
-                    ])
-                }
-
-                return image
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-    }
+  private let urlSession: URLSession
+  
+  init(urlSession: URLSession = .shared) {
+    self.urlSession = urlSession
+  }
+  
+  func publisher(for url: URL) -> AnyPublisher<UIImage, Error> {
+    urlSession.dataTaskPublisher(for: url)
+      .map(\.data)
+      .tryMap { data in
+        guard let image = UIImage(data: data), image.size.width > 0, image.size.height > 0 else {
+          throw URLError(.badServerResponse, userInfo: [
+            NSURLErrorFailingURLErrorKey: url,
+            NSLocalizedDescriptionKey: "Invalid image data"
+          ])
+        }
+        return image
+      }
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
+  }
 }
 
 class AsyncImage: ObservableObject {
   @Published var image: UIImage
   private var cancellable: AnyCancellable?
   private let imageLoader = ImageLoader()
-
+  
   init(image: UIImage) {
     self.image = image
   }
-
+  
   func loadImage(url: URL) {
     self.cancellable = imageLoader.publisher(for: url)
       .sink(receiveCompletion: { result in
@@ -52,8 +52,8 @@ class AsyncImage: ObservableObject {
         default:
           return
         }
-      }, receiveValue: { image in
-        self.image = image
+      }, receiveValue: { [weak self] image in
+        self?.image = image
       })
   }
 }
