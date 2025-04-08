47d216dab0e72b4f1421d21840fd42ecea09991e
Maurice Carrier <maurice.carrier@outlook.com>
2025-04-07 23:58:21 -0400

    Update AdaptiveShadowModifier.swift

--- Palace/Utilities/SwiftUI/AdaptiveShadowModifier.swift
+++ Palace/Utilities/SwiftUI/AdaptiveShadowModifier.swift
@@ -2,16 +2,17 @@ import SwiftUI
 
 struct AdaptiveShadowModifier: ViewModifier {
   @Environment(\.colorScheme) var colorScheme
+  var radius: CGFloat
 
   func body(content: Content) -> some View {
     content
-      .shadow(color: Color.white.opacity(0.3), radius: 5, x: -2, y: -2)
-      .shadow(color: Color.black.opacity(0.6), radius: 5, x: 2, y: 2)
+      .shadow(color: Color.white.opacity(0.3), radius: radius, x: -2, y: -2)
+      .shadow(color: Color.black.opacity(0.6), radius: radius, x: 2, y: 2)
   }
 }
 
 extension View {
-  func adaptiveShadow() -> some View {
-    self.modifier(AdaptiveShadowModifier())
+  func adaptiveShadow(radius: CGFloat = 10) -> some View {
+    self.modifier(AdaptiveShadowModifier(radius: radius))
   }
 }
