9d43ea89991128213f1a309a3135c6f4cd64536c
Maurice Carrier <maurice.carrier@outlook.com>
2025-06-20 00:20:09 -0400

    Revert TPPLCPClient changes to continue using contextQueue instead of NSLock
    This reverts commit ed8d530779408e00e2403d97acd175be4e306cda.

--- Palace/Reader2/ReaderStackConfiguration/LCP/TPPLCPClient.swift
+++ Palace/Reader2/ReaderStackConfiguration/LCP/TPPLCPClient.swift
@@ -10,48 +10,62 @@ enum LCPContextError: Error {
 
 let lcpService = LCPLibraryService()
 
+/// Facade to the private R2LCPClient.framework.
 class TPPLCPClient: ReadiumLCP.LCPClient {
 
   private var _context: LCPClientContext?
-  private let contextLock = NSLock()
+  public var context: LCPClientContext? {
+    contextQueue.sync { _context }
+  }
+
+  private let contextQueue = DispatchQueue(
+    label: "com.yourapp.tpplcpclient.contextQueue",
+    qos: .userInitiated
+  )
   
   deinit {
-    contextLock.lock()
-    _context = nil
-    contextLock.unlock()
+    contextQueue.sync {
+      _context = nil
+    }
   }
 
   func createContext(
-      jsonLicense: String,
-      hashedPassphrase: String,
-      pemCrl: String
-    ) throws -> LCPClientContext {
-      let newCtx: LCPClientContext = try {
-        guard let ctx = try? R2LCPClient.createContext(
-          jsonLicense: jsonLicense,
-          hashedPassphrase: hashedPassphrase,
-          pemCrl: pemCrl
-        ) else {
-          throw LCPContextError.creationReturnedNil
-        }
-        return ctx
-      }()
+     jsonLicense: String,
+     hashedPassphrase: String,
+     pemCrl: String
+   ) throws -> LCPClientContext {
+     var rawResult: LCPClientContext?
+     var caughtError: Error?
 
-      // 2) Store it under lock
-      contextLock.lock()
-      _context = newCtx
-      contextLock.unlock()
+     contextQueue.sync {
+       do {
+         rawResult = try R2LCPClient.createContext(
+           jsonLicense: jsonLicense,
+           hashedPassphrase: hashedPassphrase,
+           pemCrl: pemCrl
+         )
+       } catch {
+         caughtError = error
+       }
+     }
 
-      return newCtx
-    }
+     if let error = caughtError {
+       throw error
+     }
+
+     guard let newCtx = rawResult else {
+       throw LCPContextError.creationReturnedNil
+     }
+
+     contextQueue.sync {
+       self._context = newCtx
+     }
+
+     return newCtx
+   }
 
   func decrypt(data: Data, using context: LCPClientContext) -> Data? {
     guard let drmContext = context as? DRMContext else { return nil }
-
-    if Thread.isMainThread {
-      return R2LCPClient.decrypt(data: data, using: drmContext)
-    }
-
     var decrypted: Data?
     DispatchQueue.main.sync {
       decrypted = R2LCPClient.decrypt(data: data, using: drmContext)
@@ -64,14 +78,10 @@ class TPPLCPClient: ReadiumLCP.LCPClient {
   }
 }
 
+/// Provides access to data decryptor
 extension TPPLCPClient {
   func decrypt(data: Data) -> Data? {
-    guard let drmContext = _context as? DRMContext else { return nil }
-
-    if Thread.isMainThread {
-      return R2LCPClient.decrypt(data: data, using: drmContext)
-    }
-
+    guard let drmContext = context as? DRMContext else { return nil }
     var result: Data?
     DispatchQueue.main.sync {
       result = R2LCPClient.decrypt(data: data, using: drmContext)
