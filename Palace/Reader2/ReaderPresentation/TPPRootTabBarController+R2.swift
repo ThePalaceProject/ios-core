24a7b781409b6cc9bda4ebe2b39fa0813ef820a0
Maurice Carrier <maurice.carrier@outlook.com>
2025-06-24 14:11:04 -0400

    Test additional modifications

--- Palace/Reader2/ReaderPresentation/TPPRootTabBarController+R2.swift
+++ Palace/Reader2/ReaderPresentation/TPPRootTabBarController+R2.swift
@@ -1,3 +1,4 @@
+
 //
 //  TPPRootTabBarController+R2.swift
 //  The Palace Project
@@ -13,15 +14,18 @@ import Foundation
     guard let libraryService = r3Owner?.libraryService, let readerModule = r3Owner?.readerModule else {
       return
     }
-
+    
     libraryService.openBook(book, sender: self) { [weak self] result in
       guard let navVC = self?.selectedViewController as? UINavigationController else {
         preconditionFailure("No navigation controller, unable to present reader")
       }
       switch result {
       case .success(let publication):
+        self?.hideFloatingTabBarIfNeeded()
         readerModule.presentPublication(publication, book: book, in: navVC, forSample: false)
       case .failure(let error):
+        self?.restoreFloatingTabBar()
+
         // .failure is retured for an error raised while trying to unlock publication
         // error is supposed to be visible to users, it is defined by ContentProtection error property
         TPPErrorLogger.logError(error, summary: "Error accessing book resources", metadata: [
@@ -31,6 +35,22 @@ import Foundation
         TPPAlertUtils.presentFromViewControllerOrNil(alertController: alertController, viewController: self, animated: true, completion: nil)
       }
     }
+  }
+  
+  private func hideFloatingTabBarIfNeeded() {
+//    if #available(iOS 18.0, *),
+//       UIDevice.current.userInterfaceIdiom == .pad
+//    {
+//      setTabBarHidden(true, animated: false)
+//    }
+  }
+  
+  @objc private func restoreFloatingTabBar() {
+//    if #available(iOS 18.0, *),
+//       UIDevice.current.userInterfaceIdiom == .pad
+//    {
+//      setTabBarHidden(false, animated: true)
+//    }
   }
 
   func presentSample(_ book: TPPBook, url: URL) {
@@ -67,4 +87,3 @@ import Foundation
     }
   }
 }
-
