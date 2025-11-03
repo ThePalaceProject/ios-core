a0273127188cfd078193548ea902920aa7577486
Maurice Carrier <maurice.carrier@outlook.com>
2025-11-03 15:38:58 -0500

    Update TPPAppDelegate.swift

--- Palace/AppInfrastructure/TPPAppDelegate.swift
+++ Palace/AppInfrastructure/TPPAppDelegate.swift
@@ -20,7 +20,6 @@ class TPPAppDelegate: UIResponder, UIApplicationDelegate {
 
     FirebaseApp.configure()
     
-    // Initialize device-specific error monitoring
     Task {
       await DeviceSpecificErrorMonitor.shared.initialize()
     }
@@ -170,11 +169,6 @@ class TPPAppDelegate: UIResponder, UIApplicationDelegate {
     audiobookLifecycleManager.willTerminate()
     NotificationCenter.default.removeObserver(self)
     Reachability.shared.stopMonitoring()
-    
-    // BUG FIX: Don't purge caches synchronously during termination
-    // This was causing watchdog timeouts (recursive_mutex lock failed)
-    // iOS will clean up caches naturally, or we can do it on next launch
-    // MyBooksDownloadCenter.shared.purgeAllAudiobookCaches(force: false)  // ❌ Removed
   }
 
   internal func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
