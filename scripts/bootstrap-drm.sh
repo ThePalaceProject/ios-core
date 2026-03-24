e4fd46d2709312279a7433c85f2c286900a148d7
Maurice Carrier <maurice.carrier@outlook.com>
2026-03-24 17:07:55 -0400

    Update bootstrap-drm.sh

--- scripts/bootstrap-drm.sh
+++ scripts/bootstrap-drm.sh
@@ -2,14 +2,16 @@
 
 # SUMMARY
 #   Checks out all dependent repos and sets them up for developing
-#   SimplyE or Open eBooks with DRM support.
+#   Palace with DRM support.
 #
 # USAGE
 #   You only have to run this script once.
-#   Run it from the root of Simplified-iOS, e.g.:
+#   Run it from the root of ios-core, e.g.:
 #
 #     ./scripts/bootstrap-drm.sh
 #
+
+set -eo pipefail
 
 cd ..
 git clone git@github.com:ThePalaceProject/mobile-certificates.git
